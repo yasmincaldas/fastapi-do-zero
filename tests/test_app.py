107e878a3d10f58ea19be8287dd42af65f2d5a15
Yasmin Manuela <manuelayasminsc@gmail.com>
2025-04-25 09:52:59 -0300

    feat(api): Adiciona autenticação e autorização JWT

--- tests/test_app.py
+++ tests/test_app.py
@@ -152,3 +152,15 @@ def test_delete_user(client, user):
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
+
+
+def test_get_token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    token = response.json()
+
+    assert response.status_code == HTTPStatus.OK
+    assert 'access_token' in token
+    assert 'token_type' in token
